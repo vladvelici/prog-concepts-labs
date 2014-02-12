dbaef142c8a6ee8ddf83d0f7ce685aef023ee6af
Vlad Velici <vlad.velici@gmail.com>
2014-02-12 22:00:16 +0000

    implemented ch_not

--- church.ml
+++ church.ml
@@ -3,4 +3,7 @@ let ch_false (x:'a) (y:'a) = y;;
 
 let ch_and (m:('a->'a->'a)) (n:('a->'a->'a)) (x:'a) (y:'a) = m (n x y) y;;
 
+let ch_not (m:('a->'a->'a)) (x:'a) (y:'a) = m y x;;
+
 ch_and ch_true ch_false 1 2;;
+
